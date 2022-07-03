52c664031d5edcbae68c532cf0650beba24ae003
Mehmet Hüseyin Delipalta <mehmethuseyindelipalta@gmail.com>
2022-07-03 20:04:08 +0300

    Code indents edited

--- whatsapp.py
+++ whatsapp.py
@@ -8,12 +8,10 @@ time.sleep(10)
 name = input("Gönderilecek kişi adı: ")
 adet = int(input("Mesaj sayısı: "))
 mesaj = input("Mesaj: ")
-messageBox = browser.find_element_by_xpath(
-    '//span[@title = "{}"]'.format(name))
+messageBox = browser.find_element_by_xpath('//span[@title = "{}"]'.format(name))
 messageBox.click()
 time.sleep(3)
-sendMessage = browser.find_element_by_css_selector(
-    "#main > footer > div._2BU3P.tm2tP.copyable-area > div > span:nth-child(2) > div > div._2lMWa > div.p3_M1 > div > div._13NKt.copyable-text.selectable-text")
+sendMessage = browser.find_element_by_css_selector("#main > footer > div._2BU3P.tm2tP.copyable-area > div > span:nth-child(2) > div > div._2lMWa > div.p3_M1 > div > div._13NKt.copyable-text.selectable-text")
 time.sleep(3)
 for i in range(0, adet):
     sendMessage.send_keys(mesaj)
